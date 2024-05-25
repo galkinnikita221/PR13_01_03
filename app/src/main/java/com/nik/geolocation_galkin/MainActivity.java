7eb1fdf082a7576d00350a42015b8b2ecc9fae25
da15632 <vlasove156@gmail.com>
2024-05-25 09:53:49 +0500

    Разработанные переменные. Galkin

--- app/src/main/java/com/nik/geolocation_galkin/MainActivity.java
+++ app/src/main/java/com/nik/geolocation_galkin/MainActivity.java
@@ -1,14 +1,42 @@
 package com.nik.geolocation_galkin;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    LocationManager _LocationManager;
+    int ACCESS_FINE_LOCATION;
+    int ACCESS_COARSE_LOCATION;
+    TextView result;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    LocationListener _LocationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(@NonNull Location location) {
+            if (location == null) return;
+            else {
+                String message = "";
+                if (location.getProvider().equals(LocationManager.GPS_PROVIDER)) {
+                    message += "\nМестоположение определено с помощью GPS: долгота - " +
+                            location.getLatitude() + "широта - " + location.getLongitude();
+                }
+                if (location.getProvider().equals(LocationManager.NETWORK_PROVIDER)) {
+                    message += "\nМестоположение определено с помощью интернета: долгота - " +
+                            location.getLatitude() + "широта - " + location.getLongitude();
+                }
+                result.setText(message);
+            }
+        }
+    };
 }
